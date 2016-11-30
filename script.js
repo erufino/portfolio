02a8d2b7aeec230567dfa0a51d76cb5885363316
Ezra Rufino <ezrarufino@gmail.com>
2016-11-29 22:44:05 -0800

    added hamburger menu

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-script.js
+$(function() {
+  // $("a[href=#menuExpand]").click(function(e) {
+  // showHideMobile();
+  // e.preventDefault();
+  // });
+  
+  $(".mobileDimmer").click(function() {
+    showHideMobile();
+  });
+});
+
+function showHideMobile() {
+  $(".mobileMenu").toggleClass("menuOpen");
+  $(".mobileBodyWrapper").toggleClass("menuOpen");
+  $(".mobileDimmer").toggle();
+}
