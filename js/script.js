e51de772a78564b1cc96aed107edc438e06e4388
Ezra Rufino <ezrarufino@gmail.com>
2016-12-24 20:26:31 -0800

    added some more details and logos

--- js/script.js
+++ js/script.js
@@ -27,17 +27,5 @@ function showHideMobile() {
                 animatedIn:'lightSpeedIn',
                 animatedOut:'bounceOutDown',
                 color:'#3498db',
-                // Callbacks
-                beforeOpen: function() {
-                    console.log("The animation was called");
-                },           
-                afterOpen: function() {
-                    console.log("The animation is completed");
-                }, 
-                beforeClose: function() {
-                    console.log("The animation was called");
-                }, 
-                afterClose: function() {
-                    console.log("The animation is completed");
-                }
+               
             });
