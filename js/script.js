4680aad4f726672b4be4ec158c8029f2251d048e
Ezra Rufino <ezrarufino@gmail.com>
2016-12-26 14:24:56 -0800

    created 4 modals with no styling

--- js/script.js
+++ js/script.js
@@ -18,14 +18,34 @@ function showHideMobile() {
   $(".mobileDimmer").toggle();
 }
 
-      //demo 01
-            $("#demo01").animatedModal();
+            //demo 01
+            $("#demo01").animatedModal({
+                modalTarget:'modal-01',
+                animatedIn:'lightSpeedIn',
+                animatedOut:'bounceOutDown',
+                color:'#ffffff',
+            });
 
             //demo 02
             $("#demo02").animatedModal({
                 modalTarget:'modal-02',
                 animatedIn:'lightSpeedIn',
                 animatedOut:'bounceOutDown',
-                color:'#3498db',
-               
+                color:'#ffffff',
+            });
+
+            //demo 03
+            $("#demo03").animatedModal({
+                modalTarget:'modal-03',
+                animatedIn:'lightSpeedIn',
+                animatedOut:'bounceOutDown',
+                color:'#ffffff',
+            });
+
+            //demo 04
+            $("#demo04").animatedModal({
+                modalTarget:'modal-04',
+                animatedIn:'lightSpeedIn',
+                animatedOut:'bounceOutDown',
+                color:'#ffffff',
             });
